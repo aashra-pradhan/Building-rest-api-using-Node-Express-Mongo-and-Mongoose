b807332c0643cec83ce69b2770403252ce443cff
Aashra Pradhan <programmeraashra@gmail.com>
2024-05-18 22:09:03 +0545

    mongodb setup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //brain of our backend
 
 import express from "express";
+import mongoose from "mongoose";
 
 //initializing the app
 const app = express();
@@ -21,9 +22,18 @@ app.get("/", (req, res) => {
 //is a tool that helps you test your APIs, you can send requests to your server and see the response
 //without having to build the frontend.
 
-//app.listen is a method in Express.js used to start a server and listen for incoming connections on a specified port.
-// It is typically used at the end of your Express application to start the server.
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+//mongoose is a library that helps us connect to MongoDB database
+//connecting to the database
+mongoose
+  .connect(
+    "mongodb+srv://aashrapradhan68:xj2wlQU02VUUI2W3@aashradb.pkpjox1.mongodb.net/NodeAPI?retryWrites=true&w=majority&appName=AASHRADB"
+  )
+  .then(() => {
+    console.log("Connected TO THE DB!");
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+    //app.listen is a method in Express.js used to start a server and listen for incoming connections on a specified port.
+    // It is typically used at the end of your Express application to start the server.
+  })
+  .catch((err) => console.log("Connection failed"));
